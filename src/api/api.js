c2e0ee390503494042d5d09a90c664fd3eff524a
sungyu0309 <tjsrbyt@gmail.com>
2024-10-10 23:28:30 +0900

    feat : 국제선 api 연결 & 공항 코드 정보 mockApi 연결

--- src/api/api.js
+++ src/api/api.js
@@ -22,22 +22,53 @@ export const getAirportCode = async (pageNo) => {
   }
 };
 
-export const getKoreaAir = async (pageNo) => {
+export const getDomesticAirline = async (pageNo) => {
   try {
     const serviceKey = process.env.REACT_APP_API_KEY;
     const url = "service/rest/FlightScheduleList/getDflightScheduleList";
     const fullUrl = `${url}?ServiceKey=${serviceKey}&pageNo=${pageNo}`;
 
-    console.log(fullUrl);
-
     const response = await axios.get(fullUrl);
     if (response.data.response.body.items) {
       const items = response.data.response.body.items.item;
-      console.log(items);
+      const newArr = items.map((itm) => ({
+        domesticStartTime: itm.domesticStartTime,
+        domesticArrivalTime: itm.domesticArrivalTime,
+        domesticNum: itm.domesticNum,
+        startcity: itm.startcity,
+        arrivalcity: itm.arrivalcity,
+        airlineKorean: itm.airlineKorean,
+      }));
+      console.log(newArr);
     }
     return undefined;
   } catch (err) {
     console.log("ERROR : getDomesticInformation", err);
+    throw err;
+  }
+};
+
+export const getInternationalAirline = async (pageNo) => {
+  try {
+    const serviceKey = process.env.REACT_APP_API_KEY;
+    const url = "service/rest/FlightScheduleList/getIflightScheduleList";
+    const fullUrl = `${url}?ServiceKey=${serviceKey}&pageNo=${pageNo}`;
+
+    const response = await axios.get(fullUrl);
+    if (response.data.response.body.items) {
+      const items = response.data.response.body.items.item;
+      const newArr = items.map((itm) => ({
+        internationalTime: itm.internationalTime,
+        internationalNum: itm.internationalNum,
+        startcity: itm.airport,
+        arrivalcity: itm.city,
+        airlineKorean: itm.airlineKorean,
+      })); // 국제선인경우 도착 시간 X
+      console.log(newArr);
+    }
+    return undefined;
+  } catch (err) {
+    console.log("ERROR : getInternationalInformation", err);
     throw err;
   }
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,45 +1,30 @@
 import React, { useEffect, useState } from "react";
-import { getAirportCode, getKoreaAir } from "../api/api";
-import SwitchButton from "../components/Main/SwitchButton";
-import PaginationButtons from "../components/Main/PaginationButtons";
-import Props from "./Props";
-
-const airport = [
-  {
-    code: "AAA",
-    kor: "인천국제공항",
-  },
-  {
-    code: "BBB",
-    kor: "김포공항",
-  },
-];
+import { getAirportCode } from "../api/api";
+import axios from "axios";
 
 export default function Home() {
-  const [airportCodes, setAiportCodes] = useState("");
-  const [switchCheck, setSwitchCheck] = useState(true);
+  const [airportCodes, setAirportCodes] = useState("");
 
   const getApi = async () => {
-    // for (let pageNo = 1; pageNo <= 3; pageNo++) {
-    //   const newArr = await getAirportCode(pageNo);
-    //   if (newArr) {
-    //     setAiportCodes((prev) => [...prev, ...newArr]);
-    //   }
-    // }
+    for (let pageNo = 1; pageNo <= 1; pageNo++) {
+      //136
+      const newArr = await getAirportCode(pageNo);
+      if (newArr) {
+        setAirportCodes((prev) => [...prev, ...newArr]);
+      }
+    }
 
-    const response = await getKoreaAir(1);
-    console.log(response);
+    axios
+      .get("https://6707c25d8e86a8d9e42ccc3b.mockapi.io/api/airportCode")
+      .then((res) => {
+        console.log(res.data[0]);
+      })
+      .catch((err) => console.log(err));
   };
+
   useEffect(() => {
     getApi();
   }, []);
 
-  return (
-    <div>
-      <SwitchButton checked={switchCheck} setChecked={setSwitchCheck} />
-      <div style={{ display: "none" }}>
-        <PaginationButtons />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
