e0433e459e8251a77f7442ded02357678b08aeb5
sungyu0309 <tjsrbyt@gmail.com>
2024-10-10 10:25:57 +0900

    add : env 파일 && api key 저장

--- src/api/api.js
+++ src/api/api.js
@@ -1,22 +1,12 @@
 import axios from "axios";
 
-// const serviceKey =
-//   "Y39tRodvtsKfoRFJ8wqa8yuE5wJjTbDmOMNxQGDElzGcLd%2B%2BSob0U0scwH3C4kO5yO6JvcoP8mV1Gc44SU9IOw%3D%3D";
-
-// const serviceKey =
-//   "UurbbowROuq9OTKlvvbw2xIIlluYH8IEKt5J%2FXrLvn8Il4GPmsYbv4sEfJoswqv9cxxhnj%2BHn6aKqJxCzPrUuQ%3D%3D";
-
-const serviceKey =
-  "RPvte6BUUQcYUfdg1OlfZMJEmh0nAqXlSRhahPXC60outZySg4cewOug4QYKyXk8DwQBxPdzwGjGZiGZVwH4dQ%3D%3D";
-
 export const getAirportCode = async (pageNo) => {
   try {
-    console.log(serviceKey);
+    const serviceKey = process.env.REACT_APP_API_KEY;
     const url = "service/rest/AirportCodeList/getAirportCodeList";
     const fullUrl = `${url}?ServiceKey=${serviceKey}&pageNo=${pageNo}`;
 
     const response = await axios.get(fullUrl);
-    console.log(response);
     if (response.data.response.body.items) {
       const items = response.data.response.body.items.item;
       const newArr = items.map((itm) => ({
@@ -34,15 +24,20 @@ export const getAirportCode = async (pageNo) => {
 
 export const getKoreaAir = async (pageNo) => {
   try {
+    const serviceKey = process.env.REACT_APP_API_KEY;
     const url = "service/rest/FlightScheduleList/getDflightScheduleList";
     const fullUrl = `${url}?ServiceKey=${serviceKey}&pageNo=${pageNo}`;
 
     console.log(fullUrl);
 
     const response = await axios.get(fullUrl);
-    console.log(response);
+    if (response.data.response.body.items) {
+      const items = response.data.response.body.items.item;
+      console.log(items);
+    }
+    return undefined;
   } catch (err) {
-    console.log("ERROR : getAirportCode", err);
+    console.log("ERROR : getDomesticInformation", err);
     throw err;
   }
 };
--- src/pages/UserContext.js
+++
@@ -1,8 +0,0 @@
-import React, { useContext } from "react";
-
-function UserContext() {
-  const name = useContext(UserContext);
-  return <div>my name is {name}</div>;
-}
-
-export default UserContext;
