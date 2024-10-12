8d57b1d31e84891cceb861192c93e4006154a72c
sungyu0309 <tjsrbyt@gmail.com>
2024-10-12 22:52:43 +0900

    fix : 공항 코드 호출 오래걸림 이슈

--- src/components/Main/SearchInputComponent.js
+++ src/components/Main/SearchInputComponent.js
@@ -3,13 +3,39 @@ import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import Box from "@mui/material/Box";
 import { getAirportCodeList } from "../../api/api";
+import axios from "axios";
 
 export default function SearchInputComponent() {
   const [codeList, setCodeList] = useState(null);
 
-  const getApi = async () => {
-    const list = await getAirportCodeList();
-    setCodeList(list);
+  const getApi = () => {
+    const url =
+      "https://6707c25d8e86a8d9e42ccc3b.mockapi.io/api/airportCodeList";
+    axios
+      .get(url)
+      .then((res) => {
+        const seenLabels = new Set();
+
+        const newArr = res.data[0].codes
+          .filter((itm) => {
+            // 이미 존재하는 label인지 확인
+            if (seenLabels.has(itm.kor)) {
+              return false; // 중복되면 false
+            } else {
+              seenLabels.add(itm.kor); // 중복이 아니면 Set에 추가
+              return true;
+            }
+          })
+          .map((itm) => ({
+            label: itm.kor,
+            code: itm.code,
+          }));
+
+        setCodeList(newArr);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   useEffect(() => {
     getApi();
