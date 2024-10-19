028441f4e6d8a2bfc9b872ef9e00cefc34ce2d8b
sungyu0309 <tjsrbyt@gmail.com>
2024-10-19 18:51:18 +0900

    fix : error

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,19 +3,19 @@ import AirlineSearchComponent from "../components/MainPage/AirlineSearchComponen
 import AirlineListsComponent from "../components/MainPage/AirlineListsComponent";
 import { Vertical } from "../styles/CommunalStyle";
 import { Link } from "react-router-dom"; // Import Link from react-router-dom
-import AirlineSearchComponent from "../components/Main/AirlineSearchComponent";
-import AirlineListsComponent from "../components/Main/AirlineListsComponent";
 
 export default function MainPage() {
   return (
     <Vertical>
-      <div style={{ textAlign: 'center', marginTop: '20px' }}>
+      <div style={{ textAlign: "center", marginTop: "20px" }}>
         <Link to="/mypage">
-          <button style={{ padding: '10px 20px', fontSize: '16px' }}>Go to MyPage</button>
+          <button style={{ padding: "10px 20px", fontSize: "16px" }}>
+            Go to MyPage
+          </button>
         </Link>
       </div>
       <AirlineSearchComponent />
       <AirlineListsComponent />
-    </Vertical>      
+    </Vertical>
   );
 }
