6c94d26aef7469e437eb8299cb49a8e212b1a5d1
sungyu0309 <tjsrbyt@gmail.com>
2024-10-20 21:43:05 +0900

    fix : 배포 위한 오류 해결

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./Mypage.css"; 
+import "./Mypage.css";
 
 export default function Mypage() {
   const [flights, setFlights] = useState([]);
   const [flippedCards, setFlippedCards] = useState({});
   const [editFlight, setEditFlight] = useState(null);
   const [editData, setEditData] = useState({
-    title: '',
-    memo: '',
-    pic: '',
-    Departure: '',
-    Arrival: '',
-    DepartureTime: '',
-    ArrivalTime: '',
-    DepartureCode: '',
-    ArrivalCode: '',
-    AirlineName: ''
+    title: "",
+    memo: "",
+    pic: "",
+    Departure: "",
+    Arrival: "",
+    DepartureTime: "",
+    ArrivalTime: "",
+    DepartureCode: "",
+    ArrivalCode: "",
+    AirlineName: "",
   });
   const [modalOpen, setModalOpen] = useState(false);
   const [menuOpen, setMenuOpen] = useState({});
@@ -82,9 +82,11 @@ export default function Mypage() {
         `https://670731a0a0e04071d2296047.mockapi.io/flight/wanderAir/${editFlight.id}`,
         editData
       );
-      setFlights(flights.map((flight) =>
-        flight.id === editFlight.id ? { ...flight, ...editData } : flight
-      ));
+      setFlights(
+        flights.map((flight) =>
+          flight.id === editFlight.id ? { ...flight, ...editData } : flight
+        )
+      );
       setModalOpen(false);
       setEditFlight(null);
     } catch (error) {
@@ -108,12 +110,19 @@ export default function Mypage() {
         {flights.map((flight) => (
           <div
             key={flight.id}
-            className={`card-container ${flippedCards[flight.id] ? 'flipped' : ''}`}
+            className={`card-container ${
+              flippedCards[flight.id] ? "flipped" : ""
+            }`}
             onClick={() => toggleFlip(flight.id)}
           >
             <div className="card">
-              <div className="card-front" style={{ backgroundImage: `url(${flight.pic})` }}>
-                <div className="flight-codes">{flight.DepartureCode} -> {flight.ArrivalCode}</div>
+              <div
+                className="card-front"
+                style={{ backgroundImage: `url(${flight.pic})` }}
+              >
+                <div className="flight-codes">
+                  {flight.DepartureCode} {flight.ArrivalCode}
+                </div>
                 <div className="card-content">
                   <h3 className="card-title">{flight.title}</h3>
                   <p className="card-info">출발지: {flight.Departure}</p>
@@ -123,13 +132,29 @@ export default function Mypage() {
                   <p className="airline-name">{flight.AirlineName}</p>
                 </div>
                 <div className="card-menu">
-                  <div className="menu-icon" onClick={(e) => {e.stopPropagation(); toggleMenu(flight.id);}}>
+                  <div
+                    className="menu-icon"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      toggleMenu(flight.id);
+                    }}
+                  >
                     &#x22EE;
                   </div>
                   {menuOpen[flight.id] && (
                     <div className="menu-options">
-                      <button className="button edit" onClick={() => handleEdit(flight)}>수정</button>
-                      <button className="button delete" onClick={() => handleDelete(flight.id)}>삭제</button>
+                      <button
+                        className="button edit"
+                        onClick={() => handleEdit(flight)}
+                      >
+                        수정
+                      </button>
+                      <button
+                        className="button delete"
+                        onClick={() => handleDelete(flight.id)}
+                      >
+                        삭제
+                      </button>
                     </div>
                   )}
                 </div>
@@ -154,65 +179,92 @@ export default function Mypage() {
             <input
               type="text"
               value={editData.title}
-              onChange={(e) => setEditData({ ...editData, title: e.target.value })}
+              onChange={(e) =>
+                setEditData({ ...editData, title: e.target.value })
+              }
             />
             <label>Memo</label>
             <textarea
               value={editData.memo}
-              onChange={(e) => setEditData({ ...editData, memo: e.target.value })}
+              onChange={(e) =>
+                setEditData({ ...editData, memo: e.target.value })
+              }
               rows="5"
-              style={{ width: '100%' }}
+              style={{ width: "100%" }}
             />
             <label>Image URL</label>
             <input
               type="text"
               value={editData.pic}
-              onChange={(e) => setEditData({ ...editData, pic: e.target.value })}
+              onChange={(e) =>
+                setEditData({ ...editData, pic: e.target.value })
+              }
             />
             <label>Departure</label>
             <input
               type="text"
               value={editData.Departure}
-              onChange={(e) => setEditData({ ...editData, Departure: e.target.value })}
+              onChange={(e) =>
+                setEditData({ ...editData, Departure: e.target.value })
+              }
             />
             <label>Arrival</label>
             <input
               type="text"
               value={editData.Arrival}
-              onChange={(e) => setEditData({ ...editData, Arrival: e.target.value })}
+              onChange={(e) =>
+                setEditData({ ...editData, Arrival: e.target.value })
+              }
             />
             <label>Departure Time</label>
             <input
               type="text"
               value={editData.DepartureTime}
-              onChange={(e) => setEditData({ ...editData, DepartureTime: e.target.value })}
+              onChange={(e) =>
+                setEditData({ ...editData, DepartureTime: e.target.value })
+              }
             />
             <label>Arrival Time</label>
             <input
               type="text"
               value={editData.ArrivalTime}
-              onChange={(e) => setEditData({ ...editData, ArrivalTime: e.target.value })}
+              onChange={(e) =>
+                setEditData({ ...editData, ArrivalTime: e.target.value })
+              }
             />
             <label>Departure Code</label>
             <input
               type="text"
               value={editData.DepartureCode}
-              onChange={(e) => setEditData({ ...editData, DepartureCode: e.target.value })}
+              onChange={(e) =>
+                setEditData({ ...editData, DepartureCode: e.target.value })
+              }
             />
             <label>Arrival Code</label>
             <input
               type="text"
               value={editData.ArrivalCode}
-              onChange={(e) => setEditData({ ...editData, ArrivalCode: e.target.value })}
+              onChange={(e) =>
+                setEditData({ ...editData, ArrivalCode: e.target.value })
+              }
             />
             <label>Airline Name</label>
             <input
               type="text"
               value={editData.AirlineName}
-              onChange={(e) => setEditData({ ...editData, AirlineName: e.target.value })}
+              onChange={(e) =>
+                setEditData({ ...editData, AirlineName: e.target.value })
+              }
             />
-            <button className="button save" onClick={handleSaveEdit}>저장</button>
-            <button className="button cancel" onClick={() => setModalOpen(false)}>취소</button>
+            <button className="button save" onClick={handleSaveEdit}>
+              저장
+            </button>
+            <button
+              className="button cancel"
+              onClick={() => setModalOpen(false)}
+            >
+              취소
+            </button>
           </div>
         </div>
       )}
