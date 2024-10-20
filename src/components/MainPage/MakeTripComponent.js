3988044541cd77c612c1ea243dc01a3b264c339d
sungyu0309 <tjsrbyt@gmail.com>
2024-10-20 23:10:48 +0900

    fix : 사진 다중 선택 오류

--- src/components/MainPage/MakeTripComponent.js
+++ src/components/MainPage/MakeTripComponent.js
@@ -93,7 +93,6 @@ export default function MakeTripComponent({ airlineInfo, closeModal }) {
               style={{ display: "none" }}
               type="file"
               id="photo"
-              multiple
               accept="image/*"
               onChange={(e) => readURL(e)}
             />
